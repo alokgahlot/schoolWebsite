5e998b2ceea41b26b09a8e50c97b6e15570055e8
alokgahlot <alokgahlot99@gmail.com>
2022-06-04 13:39:01 +0530

    codec habd

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Service from "./pages/Service";
 import Error from "./pages/Error";
-import store from './store'
-import { Provider } from 'react-redux'
+import store from "./store";
+import { Provider } from "react-redux";
 const App = () => {
   return (
-    <Provider store={store} >
+    <Provider store={store}>
       <Switch>
-        <Route exact path="/" component={Home}></Route>
+        <Route exact path={["/schoolWebsite", "/"]} component={Home}></Route>
         <Route path="/about" component={About}></Route>
         <Route path="/contact" component={Contact}></Route>
         <Route path="/service" component={Service}></Route>
--- src/Header.js
+++ src/Header.js
@@ -1,37 +1,58 @@
 import React from "react";
-import { AiOutlineVerticalLeft , AiOutlineVerticalRight } from "react-icons/ai";
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
-import 'pure-react-carousel/dist/react-carousel.es.css';
+import { AiOutlineVerticalLeft, AiOutlineVerticalRight } from "react-icons/ai";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+import { Image } from "react-bootstrap";
+import image1 from "./image1.jfif";
 const Header = () => {
+  console.log(image1);
   return (
     <>
-
-          <CarouselProvider
+      <CarouselProvider
         naturalSlideWidth={100}
         naturalSlideHeight={40}
         totalSlides={4}
       >
         <Slider>
-  
           <Slide index={0}>
-          <img src="./images/school.jpeg" className="carrouselImage" alt="sliderImage" />
+            <img src={image1} className="carrouselImage" alt="sliderImage" />
           </Slide>
-          <Slide index={1}> <img src="./images/assembly.jpeg" className="carrouselImage" alt="sliderImage" /></Slide>
-          <Slide index={2}> <img src="./images/vacc.jpeg" className="carrouselImage" alt="sliderImage" /></Slide>
-          <Slide index={3}> <img src="./images/vacc2.jpeg" className="carrouselImage" alt="sliderImage" /></Slide>
+          <Slide index={1}>
+            <img
+              src="./images/schoolWebsite/static/media/assembly.jpeg"
+              className="carrouselImage"
+              alt="sliderImage"
+            />
+          </Slide>
+          <Slide index={2}>
+            <img
+              src="./images/vacc.jpeg"
+              className="carrouselImage"
+              alt="sliderImage"
+            />
+          </Slide>
+          <Slide index={3}>
+            <img
+              src="./images/vacc2.jpeg"
+              className="carrouselImage"
+              alt="sliderImage"
+            />
+          </Slide>
         </Slider>
-        <div
-        style={{position:'absolute', bottom:30 , left:50}}
-        >  <ButtonBack><AiOutlineVerticalRight
-        size={29}
-        
-        /></ButtonBack>
-        <ButtonNext><AiOutlineVerticalLeft
-    
-        size={29}
-      
-        /></ButtonNext></div>
-      
+        <div style={{ position: "absolute", bottom: 30, left: 50 }}>
+          <ButtonBack>
+            <AiOutlineVerticalRight size={29} />
+          </ButtonBack>
+          <ButtonNext>
+            <AiOutlineVerticalLeft size={29} />
+          </ButtonNext>
+        </div>
       </CarouselProvider>
     </>
   );
